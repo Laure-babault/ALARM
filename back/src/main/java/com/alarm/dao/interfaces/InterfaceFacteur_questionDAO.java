bb8f2216f47549e777498f0881d432c8e3cb1c2e
MaryneGuevel22 <78147665+MaryneGuevel22@users.noreply.github.com>
2023-06-02 14:44:52 +0200

    Update dao

--- back/src/main/java/com/alarm/dao/interfaces/InterfaceFacteur_questionDAO.java
+++ back/src/main/java/com/alarm/dao/interfaces/InterfaceFacteur_questionDAO.java
@@ -1,5 +1,17 @@
 package com.alarm.dao.interfaces;
 
+import java.util.List;
+
+import com.alarm.entity.Facteur_question;
+
 public interface InterfaceFacteur_questionDAO {
+	
+	Facteur_question getById(int id);
+
+	List<Facteur_question> getAll();
+
+	void addFacteurQuestion(Facteur_question facteur_question);
+
+	void updateFacteurQuestion(Facteur_question facteur_question);
 
 }
